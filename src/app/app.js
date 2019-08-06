d87668c5a3d358b27d5366cfaeb048af85437484
Fdcoder81 <carloanselmi81@gmail.com>
2019-08-06 16:34:05 +0200

    Added media queries

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,22 @@
 const iconRight = document.querySelector(".slider-icon-right");
 const iconLeft = document.querySelector(".slider-icon-left");
 const sliderCards = document.querySelectorAll(".courses__list--slider-card");
+const toggleContainer = document.querySelector(".toggle__container");
+const toggle = document.querySelector(".toggle__icon");
+const toggleContent = document.querySelector(".toggle__content");
+const toggleClose = document.querySelector(".toggle__icon-close");
+
+toggleContainer.addEventListener("click", e => {
+  if (toggleContent.classList.value.includes("hidden")) {
+    toggleContent.classList.remove("hidden");
+    toggle.classList.add("hidden");
+    toggleClose.classList.remove("hidden");
+  } else {
+    toggleContent.classList.add("hidden");
+    toggle.classList.remove("hidden");
+    toggleClose.classList.add("hidden");
+  }
+});
 
 iconRight.addEventListener("click", e => {
   iconLeft.classList.remove("hidden");
